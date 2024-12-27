a8d0d84a9f4b2d45ac4c5839fed1cfd94f5ba73b
subedisuraj1 <sr.subedi1@gmail.com>
2024-12-27 07:31:13 +0545

    first project completed

--- lib/child_three.dart
+++ lib/child_three.dart
@@ -5,40 +5,41 @@ class ChildThree extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(20),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.blueGrey[900],
-          borderRadius: BorderRadius.circular(30),
-        ),
-        child: Row(
-          children: [
-            Image.asset(
-              "assets/images/luffy.png",
-              height: 50,
-            ),
-            Column(
-              spacing: 3,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Alex Bristol",
-                  style: TextStyle(
-                      color: Colors.white, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  "Flutter Developer",
-                  style: TextStyle(color: Colors.white),
-                ),
-                Text(
-                  "www.alexbristol.com",
-                  style: TextStyle(color: Colors.white),
-                ),
-              ],
-            )
-          ],
-        ),
+    return Container(
+      padding: EdgeInsets.fromLTRB(70, 20, 20, 20),
+      decoration: BoxDecoration(
+        color: Colors.blueGrey[900],
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: Row(
+        children: [
+          Image.asset(
+            "assets/images/luffy.png",
+            height: 50,
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          Column(
+            spacing: 3,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Alex Bristol",
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "Flutter Developer",
+                style: TextStyle(color: Colors.white),
+              ),
+              Text(
+                "www.alexbristol.com",
+                style: TextStyle(color: Colors.white),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
